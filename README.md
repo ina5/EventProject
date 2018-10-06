cb35a27620c55b754aa0601747021c8bf3d865e6
Maria Dineva <stoychevamaria89@gmail.com>
2018-10-06 17:43:26 +0300

    first part of database

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # EventProject
 
-Our team's name is M2V. 
-Our team leader is Milen.
-Our team members are Milen Rabadzhiev, Vanina Panova and Maria Dineva.
+Our team's name is M2V and our team leader is Milen.
+The members of the team are Milen Rabadzhiev, Vanina Panova and Maria Dineva.
 Our project is going to be about creating, displaying, searching and deleting events of four types - music, culture, sport and business. They are going to be divided into past, current and future events.
 We are going to have a pre-prepared database. Each event is going to have the
 following features: id, title, description, type, picture, location, date/time.
---
+++ js/database.js
@@ -0,0 +1,34 @@
+const events = [
+    {
+        id: 1,
+        title: 'Stone Sour Concert',
+        description: 'Stone Sour playing live in Sofia. Special guests: "Nothing More" and "Dash the effort"',
+        type: 'music',
+        picture: './../images/StoneSour.jpeg',
+        location: 'Inter Expo Center',
+        dateTime: '27/06/2018 19:00',
+    },
+    {
+        id: 2,
+        title: 'Levski vs Slavia Football Match',
+        description: 'A football match between two of the leading Bulgarian teams',
+        type: 'sport',
+        picture: './../images/slavia-levski.jpg',
+        location: 'Georgi Asparuhov stadium',
+        dateTime: '07/10/2018 19:15',
+    },
+    {
+        id: 3,
+        title: '5-th Comedy Festival Sofia',
+        description: 'The fifth festival of comedy will be the biggest stand-up comdey show ever done in Bulgaria',
+        type: 'culture',
+        picture: './../images/Comedy-Fest.jpg',
+        location: 'National Palace of Culture, Hall 1',
+        dateTime: '15/10/2018 20:00',
+    },
+    {
+        id: 4,
+        title: ''
+    }
+
+]
