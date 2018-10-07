45cc46ec3420ccd381fe96112282c522bc2e002b
Milen Rabadzhiev <milenrab97@gmail.com>
2018-10-07 23:33:48 +0300

    Displayed events on different tabs with JQuery; fixed background img

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import * as EventFactory from './EventFactory.js';
 import {
+  log
+} from './logger.js';
+import {
   searchManager
 } from './SearchManager.js';
 const event = EventFactory.createEvent(1, 2, 3, 4, 5, 6);
@@ -9,15 +12,17 @@ console.log(person);
 events.push(person);
 console.log(person2); */
 
-EventFactory.addEventToDB(event);
+//EventFactory.addEventToDB(event);
 
 // Display an array with our events in
 
 if (!localStorage.getItem(!'events')) {
   localStorage.setItem('events', JSON.stringify(EventFactory.all()));
 } else {
-  console.log(JSON.parse(localStorage.getItem('events')));
+  log(JSON.parse(localStorage.getItem('events')));
 }
+
+
 // get the elements, which user provided in html
 const searchDropDown = document.getElementById("searchDropDown");
 const searchTextElement = document.getElementById("searchString");
@@ -29,6 +34,62 @@ searchDropDown.addEventListener("click", function (ev) {
   // check if we don't find nothing
   if (foundEvents.length === 0) {
     // This log will be on alert message
-    console.log('No matches found!')
+    log('No matches found!')
   }
+});
+
+
+//TO DO:: DO IT IN ANOTHER MODULE
+
+//TODO: change the return condition (compare to current date and not
+// the magic number for month 10)
+
+const eventsToday = EventFactory.all()
+.filter(event => {
+  return +event.dateTime.split(' ')[0].split('/')[1] === 10;
+});
+
+const eventsPast = EventFactory.all()
+.filter(event => {
+  return +event.dateTime.split(' ')[0].split('/')[1] < 10;
+});
+
+const eventsFuture = EventFactory.all()
+.filter(event => {
+  return +event.dateTime.split(' ')[0].split('/')[1] > 10;
+});
+
+eventsToday.forEach(event => {
+//log(event.title);
+$(".event-list").append(`<li> ${event.title} </li>`).show();
+});
+
+$(".topnav li").on('click', function () {
+//log($(this).html());
+
+//make the last active -> inactive
+$(this).parent().find("li").removeClass("active-tab inactive-tab").addClass("inactive-tab");
+
+//make clicked one active
+$(this).removeClass("inactive-tab").addClass("active-tab");
+
+//update displayed events
+const toDisplayStatus = $(this).find('a').attr('href').slice(1);
+$(".event-list").empty();
+if (toDisplayStatus === 'today') {
+  eventsToday.forEach(event => {
+    //log(event.title);
+    $(".event-list").append(`<li> ${event.title} </li>`).show();
+  });
+} else if (toDisplayStatus === 'past') {
+  eventsPast.forEach(event => {
+    //log(event.title);
+    $(".event-list").append(`<li> ${event.title} </li>`).show();
+  });
+} else {
+  eventsFuture.forEach(event => {
+    //log(event.title);
+    $(".event-list").append(`<li> ${event.title} </li>`).show();
+  });
+}
 });
