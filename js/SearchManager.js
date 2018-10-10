852b76740c148eb588f7e157b1a239cdbb8df919
Milen Rabadzhiev <milenrab97@gmail.com>
2018-10-10 21:52:24 +0300

    Minor ESlint fix

--- js/SearchManager.js
+++ js/SearchManager.js
@@ -2,13 +2,13 @@ import eventsLocalStorage from './database.js';
 // Create new Object with different properties
 
 // seach a string from text box by criteria from drop down
-const find = function (searchText, typeDd) {
+const find = function(searchText, typeDd) {
   if (searchText === '' || !typeDd) {
     throw new Error('No valid data provided');
   }
   return eventsLocalStorage
-    .filter((event) => event[typeDd].toLowerCase().includes(searchText.toLowerCase()));
+      .filter((event) => event[typeDd].toLowerCase().includes(searchText.toLowerCase()));
 };
 export {
-  find
+  find,
 };
